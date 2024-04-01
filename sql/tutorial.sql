9356dfcddc25a1c332691d5839284555b23ed2a4
hoc de lay bang <duyanvu2409@gmail.com>
2024-04-02 03:52:26 +0700

    demo2

--- sql/tutorial.sql
+++ sql/tutorial.sql
@@ -2,6 +2,7 @@ CREATE TABLE users(
     Id int PRIMARY KEY AUTO_INCREMENT,
     Username varchar(200),
     Email varchar(200),
-    Age int,
-    Password varchar(200)
+    dateofbirth DATE NULL,
+    Password varchar(200),
+    create_at DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
